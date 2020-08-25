d49ce2d2b701c8808d241dd2c7fb07a268ab2ce1
dbc2201 <divyanshb22@gmail.com>
2020-08-25 11:41:15 +0530

    add getter and setter methods for the private field

--- src/definitions/Phone.java
+++ src/definitions/Phone.java
@@ -9,4 +9,12 @@ package definitions;
 // landline; tethered to a wall outlet.
 public class Phone {
     private long contactNumber; // the contact number of the phone itself.
+
+    public long getContactNumber() {
+        return contactNumber;
+    }
+
+    public void setContactNumber(long contactNumber) {
+        this.contactNumber = contactNumber;
+    }
 }
