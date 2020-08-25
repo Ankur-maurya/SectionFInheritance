735d774ccef81c7dd87072f896d36aa1ab487bd4
dbc2201 <divyanshb22@gmail.com>
2020-08-25 11:48:08 +0530

    extend Phone.java from MobilePhone.java

--- src/definitions/MobilePhone.java
+++ src/definitions/MobilePhone.java
@@ -7,11 +7,18 @@
 package definitions;
 
 // a simple mobile phone like Noika 1100
-public class MobilePhone {
-
+public class MobilePhone extends Phone {
+    // MobilePhone extends Phone
+    // MobilePhone IS A type of Phone
+    public void printContactNumber() {
+        System.out.println(getContactNumber());
+    }
 }
 
 // Two entities; every entity is a class,
 // first entity (class) : Phone.java class
 // second entity (class) : MobilePhone.java class
 // is somehow relate the two classes (entities)
+
+// MobilePhone IS A type of Phone.
+// Phone should have been invented first, and then we (humans) invented a Mobile Phone.
