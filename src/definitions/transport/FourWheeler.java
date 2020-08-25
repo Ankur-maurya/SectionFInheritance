df2185904ac579dc46646ad25b2d8a69c6702797
dbc2201 <divyanshb22@gmail.com>
2020-08-25 13:12:15 +0530

    create FourWheeler.java

---
+++ src/definitions/transport/FourWheeler.java
@@ -0,0 +1,12 @@
+/*  Created by IntelliJ IDEA.
+ *  User: Divyansh Bhardwaj (dbc2201)
+ *  Date: 25/08/20
+ *  Time: 1:11 PM
+ *  File Name : FourWheeler.java
+ * */
+package definitions.transport;
+
+// This will be the subclass (child class) for our example.
+public class FourWheeler {
+
+}
