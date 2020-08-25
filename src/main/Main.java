7bb61099fae5e2e629f817a8533a818754340572
dbc2201 <divyanshb22@gmail.com>
2020-08-25 11:50:35 +0530

    create Main.java with example of importing the Scanner class

---
+++ src/main/Main.java
@@ -0,0 +1,17 @@
+/*  Created by IntelliJ IDEA.
+ *  User: Divyansh Bhardwaj (dbc2201)
+ *  Date: 25/08/20
+ *  Time: 11:48 AM
+ *  File Name : Main.java
+ * */
+package main;
+
+import java.util.Scanner;
+
+public class Main {
+    // Main IS A type of Scanner, true?
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        scanner.close();
+    }
+}
