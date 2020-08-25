c8330d42fa75ae06719eb823c17f4cd894d6f9c6
dbc2201 <divyanshb22@gmail.com>
2020-08-25 11:24:06 +0530

    write definitions for Inheritance in Simple English and Object-Oriented

--- src/inheritance/InheritanceDemo.java
+++ src/inheritance/InheritanceDemo.java
@@ -8,6 +8,23 @@ package inheritance;
 
 public class InheritanceDemo {
     // 1. What is Inheritance?
+
+    // Inheritance (Simple English):
+    // Inheritance (noun): something that is or may be "inherited".
+    // Inheritance (verb): to derive something.
+
+    // Inheritance (verb) is the act of acquiring some physical attributes/properties/
+    // behavior/traits/possessions by ONE ENTITY from ANOTHER ENTITY related to it.
+
+    // Some examples from our daily lives:
+    // 1. YOU have "inherited" (acquired) your physical attributes from YOUR PARENTS,
+    // ex - hair color, color of eyes, height, ... etc.
+    // 2. YOU might get some money, a house, a car, ... etc from YOUR PARENTS if they choose to.
+
+    // Inheritance (Object-Oriented): Every entity is a class!
+    // Inheritance (verb) is the mechanism by which a class can acquire
+    // properties(fields/variables) and methods of another class, related to it.
+
     // 2. How to use Inheritance to re-use code/programs/software that is already developed and tested.
     // 3. Observe the `extends` keyword.
     // 4. What is a `super` class and what is `sub` class.
