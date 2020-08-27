8ddb802c54821aab1c6784ad09df6f431b17f358
dbc2201 <divyanshb22@gmail.com>
2020-08-27 16:08:25 +0530

    create Truck.java

---
+++ src/definitions/transport/Truck.java
@@ -0,0 +1,10 @@
+/*  Created by IntelliJ IDEA.
+ *  User: Divyansh Bhardwaj (dbc2201)
+ *  Date: 27/08/20
+ *  Time: 4:08 PM
+ *  File Name : Truck.java
+ * */
+package definitions.transport;
+
+public class Truck {
+}
