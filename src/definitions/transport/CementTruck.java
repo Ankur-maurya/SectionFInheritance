1f55d8182d73e2c38ccd290ca76096f6560582d1
dbc2201 <divyanshb22@gmail.com>
2020-08-27 16:16:31 +0530

    create CementTruck.java and access the Truck class

---
+++ src/definitions/transport/CementTruck.java
@@ -0,0 +1,12 @@
+/*  Created by IntelliJ IDEA.
+ *  User: Divyansh Bhardwaj (dbc2201)
+ *  Date: 27/08/20
+ *  Time: 4:15 PM
+ *  File Name : CementTruck.java
+ * */
+package definitions.transport;
+
+public class CementTruck {
+    // we can access the Truck class in a different class, but in the same package.
+    private Truck myCementTruck;
+}
