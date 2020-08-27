e3cca61953d4a8b457e7da1b8961571edd8da3bd
dbc2201 <divyanshb22@gmail.com>
2020-08-27 16:15:11 +0530

    access the Truck class in the same file

--- src/definitions/transport/Truck.java
+++ src/definitions/transport/Truck.java
@@ -14,3 +14,7 @@ package definitions.transport;
 public class Truck extends Vehicle {
 
 }
+class DeliveryTruck {
+    // we can access the Truck class in the same file.
+    private Truck myTruck;
+}
