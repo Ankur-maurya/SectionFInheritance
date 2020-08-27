db3f30d75c5094033556aba57541e5a16446eb90
dbc2201 <divyanshb22@gmail.com>
2020-08-27 16:17:42 +0530

    create MonsterTruck.java and access the Truck class outside the package

---
+++ src/definitions/MonsterTruck.java
@@ -0,0 +1,15 @@
+/*  Created by IntelliJ IDEA.
+ *  User: Divyansh Bhardwaj (dbc2201)
+ *  Date: 27/08/20
+ *  Time: 4:16 PM
+ *  File Name : MonsterTruck.java
+ * */
+package definitions;
+
+import definitions.transport.Truck;
+
+public class MonsterTruck {
+    // we can access the Truck class
+    // in a different class, even outside the package!
+    private Truck myMonsterTruck;
+}
