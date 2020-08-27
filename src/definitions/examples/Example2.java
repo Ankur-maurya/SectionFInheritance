a018fbea5c7651917a13e21079dc407f24bf685e
dbc2201 <divyanshb22@gmail.com>
2020-08-27 16:49:03 +0530

    add methods to class A and B

--- src/definitions/examples/Example2.java
+++ src/definitions/examples/Example2.java
@@ -15,12 +15,16 @@ class A {
     int j = 0;
     public void methodA(int k) {
         j = k;
-        System.out.println("j = " + j);
+        System.out.println("Class A : j = " + j);
     }
 }
 class B extends A {
     public void methodA(int x) {
-
+        j = x * x;
+        System.out.println("Class B : j = " + j);
+    }
+    public void methodB() {
+        System.out.println("Class B");
     }
 }
 
