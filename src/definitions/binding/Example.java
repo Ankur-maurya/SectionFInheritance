f864a6153669d03f7d0de80622a31ca0d301e43e
dbc2201 <divyanshb22@gmail.com>
2020-09-01 11:05:08 +0530

    create Example.java

---
+++ src/definitions/binding/Example.java
@@ -0,0 +1,10 @@
+/*  Created by IntelliJ IDEA.
+ *  User: Divyansh Bhardwaj (dbc2201)
+ *  Date: 01/09/20
+ *  Time: 11:04 AM
+ *  File Name : Example.java
+ * */
+package definitions.binding;
+
+public class Example {
+}
