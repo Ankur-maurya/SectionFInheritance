3b62c41abd481b4d8d3cd331e8d7af89024e08cb
dbc2201 <divyanshb22@gmail.com>
2020-09-04 13:16:46 +0530

    create Bank Interface in Bank.java

---
+++ src/interfaces/Bank.java
@@ -0,0 +1,10 @@
+/*  Created by IntelliJ IDEA.
+ *  User: Divyansh Bhardwaj (dbc2201)
+ *  Date: 04/09/20
+ *  Time: 1:15 PM
+ *  File Name : Bank.java
+ * */
+package interfaces;
+
+public interface Bank {
+}
