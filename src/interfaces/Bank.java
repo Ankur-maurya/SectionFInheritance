f210d0dfdfc1e2073913c9ca4cd6f950e0d28552
dbc2201 <divyanshb22@gmail.com>
2020-09-04 13:23:41 +0530

    add rules for fields and methods of an interface

--- src/interfaces/Bank.java
+++ src/interfaces/Bank.java
@@ -7,4 +7,24 @@
 package interfaces;
 
 public interface Bank {
+
+    // 1. Fields
+    // i. all the fields declared inside an interface must be initialized.
+    // ii. all the fields declared inside an interface are implicitly declared
+    // as public.
+    // iii. all the fields declared inside an interface are implicitly declared
+    // as final i.e we cannot change its value after is has initialized.
+    // iv. all the fields declared inside an interface are implicitly declared
+    // as static.
+    // public static final <dataType> <variableName> = <initialValue>;
+    public static final int number = 4;
+
+    // 2. Methods
+    // i. all the method declared inside an interface are implicitly declared
+    // as abstract.
+    // ii. all the methods declared inside an interface are implicitly declared
+    // as public.
+    // public abstract <dataType> <methodName>(<Parameters>);
+    public abstract void method();
+
 }
